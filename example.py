21dd490a49cd60c033a98a36522675bf015c39d5
nullt3r <100472242+nullt3r@users.noreply.github.com>
2023-07-25 19:15:28 +0200

    Create example.py

---
+++ example.py
@@ -0,0 +1,39 @@
+from uwebserver import UWebServer
+import network
+from time import sleep
+from picozero import pico_led
+import ujson
+import _thread
+
+web_server = UWebServer(port=80, static_dir='static')
+
+@web_server.route("/api/led", "POST")
+def api_led_control(data):
+    state = ujson.loads(data)
+
+    if 'led' in state:
+        if state["led"] == 1:
+            pico_led.on()
+        elif state["led"] == 0:
+            pico_led.off()
+
+        return ujson.dumps({"message": f"LED set to {state['led']}"}), "application/json", "200 OK"
+    else:
+        return ujson.dumps({"message": "No LED state in data"}), "application/json", "200 OK"
+
+if __name__ == "__main__":
+    ssid = "Your wifi"
+    password = "password"
+
+    ap = network.WLAN(network.STA_IF)
+    ap.active(True)
+    ap.connect(ssid, password)
+
+    while ap.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+
+    print('Connection is successful')
+    print(ap.ifconfig())
+
+    web_server.start()
